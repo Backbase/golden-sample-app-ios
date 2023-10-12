4ea309f0683dcd6bd870854ce70f24d1f64d4d6f
George Nyakundi <dev.things.ke@gmail.com>
2023-10-12 11:39:04 +0200

    Add TermDeposits and AccountSummary

---
+++ Sources/Journeys/AccountsJourney/Models/TermDeposit/TermDeposits.swift
@@ -0,0 +1,39 @@
+//
+//  TermDeposits.swift
+//  GoldenSampleApp
+//
+//  Created by George Nyakundi on 12/10/2023.
+//
+
+import Foundation
+
+
+struct TermDeposits: Equatable {
+    /// The list of term deposit.
+    let products: [TermDeposit]
+    /// The label/name for term deposits.
+    let name: String?
+    /// The aggregated balance of this group of products.
+    let aggregatedBalance: AggregatedBalance?
+    /// Extra parameters
+    let additions: [String: String]
+    
+    /// Creates a `TermsDeposits` object
+    /// - Parameters:
+    ///   - products: The list of term deposit.
+    ///   - name: The label/name for term deposits.
+    ///   - aggregatedBalance: The aggregated balance of this group of products.
+    ///   - additions: Extra parameters
+    init(
+        products: [TermDeposit],
+        name: String?,
+        aggregatedBalance: AggregatedBalance?,
+        additions: [String : String]
+    ) {
+        self.products = products
+        self.name = name
+        self.aggregatedBalance = aggregatedBalance
+        self.additions = additions
+    }
+    
+}
