dbb0a457158fc5f088902f6f339c6e302ec0cc87
George Nyakundi <dev.things.ke@gmail.com>
2024-02-20 18:28:46 +0300

    Simplify tests as this is to be used a guide

--- Tests/accounts-journey/unit-tests/AccountsJourneyObservabilityTests.swift
+++ Tests/accounts-journey/unit-tests/AccountsJourneyObservabilityTests.swift
@@ -13,75 +13,69 @@ final class AccountsJourneyObservabilityTests: XCTestCase {
         Resolver.register { DummyAccountsListUseCase() as AccountsListUseCase }
         Resolver.register { DummyAccountDetailsUseCase() as AccountDetailsUseCase }
     }
-   
     
     //MARK: -  AccountsList Events
     
-    func test_noUserActionEventIsPublished_whenAccountsListViewModel_is_initialized() {
-        let userActionSpy = TrackerSpy<UserActionEvent>()
-        expect(userActionSpy, toReceive: [], when: {
-            _ = AccountsListViewModel()
-        })
-    }
-    
-    func test_noScreenViewEventIsPublished_whenAccountListViewModel_is_initialized() {
-        let screenViewEventsSpy = TrackerSpy<ScreenViewEvent>()
-        expect(screenViewEventsSpy, toReceive: [], when: {
-            _ = AccountsListViewModel()
-        })
-    }
-    
     func test_accountsScreenEventIsPublished_whenTheAccountsViewLoads() {
         let screenViewEventsSpy = TrackerSpy<ScreenViewEvent>()
+        Resolver.register { screenViewEventsSpy as Tracker }
         
-        expect(screenViewEventsSpy, toReceive: [ScreenViewEvent(.accounts_list)], when: {
-            AccountsListViewModel().onEvent(.getAccounts)
-            let viewDidAppearSubject = PassthroughSubject<Void, Never>()
-            let userActionEventSubject = PassthroughSubject<UserActionEvent, Never>()
-            let viewModel = AccountsListViewModel()
-            viewModel.bind(viewDidAppearPublisher: viewDidAppearSubject.eraseToAnyPublisher(), userActionEventPublisher: userActionEventSubject.eraseToAnyPublisher())
-            viewDidAppearSubject.send()
-        })
+        let viewDidAppearSubject = PassthroughSubject<Void, Never>()
+        let userActionEventSubject = PassthroughSubject<UserActionEvent, Never>()
+        let viewModel = AccountsListViewModel()
+        
+        viewModel.bind(
+            viewDidAppearPublisher: viewDidAppearSubject.eraseToAnyPublisher(),
+            userActionEventPublisher: userActionEventSubject.eraseToAnyPublisher()
+        )
+        
+        // No event has been triggered yet. List of events should be empty
+        XCTAssertEqual(screenViewEventsSpy.publishedEvents, [])
+        
+        viewDidAppearSubject.send()
+        
+        XCTAssertEqual(screenViewEventsSpy.publishedEvents, [ScreenViewEvent(.accounts_list)])
     }
     
     func test_refresh_accountsUserActionEvent_isPublished_whenTheRefreshEventIsEmitted() {
         let userActionSpy = TrackerSpy<UserActionEvent>()
         
-        expect(userActionSpy, toReceive: [UserActionEvent(.refresh_accounts)], when: {
-            
-            let viewDidAppearSubject = PassthroughSubject<Void, Never>()
-            let userActionEventSubject = PassthroughSubject<UserActionEvent, Never>()
-            let viewModel = AccountsListViewModel()
-            viewModel.bind(viewDidAppearPublisher: viewDidAppearSubject.eraseToAnyPublisher(), userActionEventPublisher: userActionEventSubject.eraseToAnyPublisher())
-            userActionEventSubject.send(UserActionEvent(.refresh_accounts))
-        })
+        Resolver.register { userActionSpy as Tracker }
+        
+        let viewDidAppearSubject = PassthroughSubject<Void, Never>()
+        let userActionEventSubject = PassthroughSubject<UserActionEvent, Never>()
+        let viewModel = AccountsListViewModel()
+        
+        viewModel.bind(
+            viewDidAppearPublisher: viewDidAppearSubject.eraseToAnyPublisher(),
+            userActionEventPublisher: userActionEventSubject.eraseToAnyPublisher()
+        )
+        
+        // No event has been triggered yet. List of events should be empty
+        XCTAssertEqual(userActionSpy.publishedEvents, [])
+        
+        userActionEventSubject.send(UserActionEvent(.refresh_accounts))
+        
+        XCTAssertEqual(userActionSpy.publishedEvents, [UserActionEvent(.refresh_accounts)])
     }
     
 
     // MARK: - AccountDetails Tests
-    func test_noUserActionEventIsPublished_whenAccountDetailsViewModel_is_initialized() {
-        let userActionSpy = TrackerSpy<UserActionEvent>()
-        expect(userActionSpy, toReceive: [], when: {
-            _ = AccountDetailsViewModel()
-        })
-    }
-    
-    func test_noScreenViewEventIsPublished_whenAccountDetailsViewModel_is_initialized() {
-        let screenViewEventsSpy = TrackerSpy<ScreenViewEvent>()
-        expect(screenViewEventsSpy, toReceive: [], when: {
-            _ = AccountDetailsViewModel()
-        })
-    }
     
     func test_accountsDetailsScreenEventIsPublished_whenTheAccountsDetailsViewLoads() {
         let screenViewEventsSpy = TrackerSpy<ScreenViewEvent>()
-
-        expect(screenViewEventsSpy, toReceive: [ScreenViewEvent(.account_details)], when: {
-            let valueSubject = PassthroughSubject<Void, Never>()
-            let viewModel = AccountDetailsViewModel()
-            viewModel.bind(viewDidAppear: valueSubject.eraseToAnyPublisher())
-            valueSubject.send()
-        })
+        Resolver.register { screenViewEventsSpy as Tracker }
+        
+        let valueSubject = PassthroughSubject<Void, Never>()
+        let viewModel = AccountDetailsViewModel()
+        viewModel.bind(viewDidAppear: valueSubject.eraseToAnyPublisher())
+        
+        // No event has been triggered yet. List of events should be empty
+        XCTAssertEqual(screenViewEventsSpy.publishedEvents, [])
+        
+        valueSubject.send()
+        
+        XCTAssertEqual(screenViewEventsSpy.publishedEvents, [ScreenViewEvent(.account_details)])
     }
     
     // MARK: - Private
@@ -97,17 +91,6 @@ final class AccountsJourneyObservabilityTests: XCTestCase {
         func unsubscribe(subscriber: AnyHashable) {}
     }
     
-    private func expect<S>(_ sut: TrackerSpy<S>, toReceive events: [S?], when action: () -> Void, file: StaticString = #file, line: UInt = #line) where S: Equatable {
-        let spy = TrackerSpy<S>()
-        Resolver.register { spy as Tracker }
-        
-        XCTAssertEqual(spy.publishedEvents, [], "No events should be published before the action() closure.", file: file, line: line)
-        
-        action()
-        
-        XCTAssertEqual(spy.publishedEvents, events, file: file, line: line)
-    }
-    
 }
 extension ScreenViewEvent: Equatable {
     public static func == (lhs: ScreenViewEvent, rhs: ScreenViewEvent) -> Bool {
@@ -116,13 +99,9 @@ extension ScreenViewEvent: Equatable {
 }
 
 private final class DummyAccountsListUseCase: AccountsListUseCase {
-    func getAccountSummary(_ completion: @escaping (Result<AccountsJourney.AccountsSummary, AccountsJourney.ErrorResponse>) -> Void) {
-        
-    }
+    func getAccountSummary(_ completion: @escaping (Result<AccountsJourney.AccountsSummary, AccountsJourney.ErrorResponse>) -> Void) {}
 }
 
 private final class DummyAccountDetailsUseCase: AccountDetailsUseCase {
-    func getAccountDetail(arrangementId: String, _ completion: @escaping (Result<AccountsJourney.AccountDetailsModel, AccountsJourney.ErrorResponse>) -> Void) {
-        
-    }
+    func getAccountDetail(arrangementId: String, _ completion: @escaping (Result<AccountsJourney.AccountDetailsModel, AccountsJourney.ErrorResponse>) -> Void) {}
 }
