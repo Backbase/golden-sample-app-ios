238d8c4845eb2f7e938d58aea34daa850fac456a
George Nyakundi <georgen@backbase.com>
2024-03-14 12:11:08 +0100

    Add Snapshots for happy and sad paths

--- Tests/SnapshotTests/AccountDetailsScreen.swift
+++ Tests/SnapshotTests/AccountDetailsScreen.swift
@@ -9,38 +9,88 @@ import Backbase
 //@testable import GoldenSampleApp
 
 class AccountDetailsScreenSnapshotTests: XCTestCase {
-    // MARK: Properties
-    lazy var productSummaryClient = clientFactory(ArrangementsClient2Gen2.ProductSummaryAPI(), "api/arrangement-manager")
-    lazy var arrangementsClient = clientFactory(ArrangementsClient2Gen2.ArrangementsAPI(), "api/arrangement-manager")
-
-    func clientFactory<T: DBSClient>(_ client: T, _ path: String) -> T {
-        guard let serverURL: URL = URL(string: Backbase.configuration().backbase.serverURL) else {
-            fatalError("Invalid or no serverURL found in the SDK configuration")
-        }
-        client.baseURL = serverURL.appendingPathComponent(path)
-        
-        if let dataProvider = Resolver.optional(DBSDataProvider.self) {
-            client.dataProvider = dataProvider
-            return client
-        } else {
-            try? Backbase.register(client: client)
-            guard let dbsClient = Backbase.registered(client: T.self),
-                  let client = dbsClient as? T else {
-                fatalError("Failed to retrieve \(T.self) client")
-            }
-            return client
-        }
+    override func setUp() {
+//        isRecording = true
     }
 
     func testSuggestionsHappyPath() {
-        Resolver.register { AccountsListSummaryUseCaseImp(client: self.productSummaryClient) as AccountsListUseCase }
-        Resolver.register { AccountDetailUseCaseImp(client: self.arrangementsClient) as AccountDetailsUseCase}
+        
         Resolver.register { AccountsJourney.Configuration() }
+        
         let viewModel = AccountDetailsViewModel()
+        // Register the Mock
+        Resolver.register { MockAccountDetailsUseCase() as AccountDetailsUseCase }
+        
         let viewController = AccountDetailsViewController(viewModel: viewModel, arrangementId: "")
+        
+        // fire the event that fetches account details
+        viewController.viewModel.onEvent(.getAccountDetails(""))
         
         let result = verifyViewSnapshot(with: viewController.view)
                 XCTAssertTrue(result.isEmpty, "Failed in \(#function) snapshot test")
         
     }
+    
+    func testSuggestionsSadPath() {
+        
+        Resolver.register { AccountsJourney.Configuration() }
+        
+        let viewModel = AccountDetailsViewModel()
+        // Register the Mock
+        Resolver.register { MockAccountDetailsUseCase(shouldReturnError: true) as AccountDetailsUseCase }
+        
+        let viewController = AccountDetailsViewController(viewModel: viewModel, arrangementId: "")
+        
+        // fire the event that fetches account details
+        viewController.viewModel.onEvent(.getAccountDetails(""))
+        
+        let result = verifyViewSnapshot(with: viewController.view)
+        XCTAssertTrue(result.isEmpty, "Failed in \(#function) snapshot test")
+        
+    }
 }
+
+private extension Bundle {
+    static func data(from path: String) -> Data {
+        guard let filePath = Bundle(for: AccountDetailsScreenSnapshotTests.self).path(forResource: path, ofType: nil) else {
+            fatalError("Failed to find file \(path)")
+        }
+        let fileUrl = URL(fileURLWithPath: filePath)
+        do {
+            let data = try Data(contentsOf: fileUrl, options: .uncached)
+            return data
+        } catch {
+            fatalError("Failed to find file \(path)")
+        }
+    }
+}
+
+
+class MockAccountDetailsUseCase: AccountDetailsUseCase {
+    private var shouldReturnError = false
+    private var fileName: String
+    
+    init(shouldReturnError: Bool = false, fileName: String = "arrangement.json") {
+        self.shouldReturnError = shouldReturnError
+        self.fileName = fileName
+    }
+    
+    func getAccountDetail(
+        arrangementId: String,
+        _ completion: @escaping (Result<AccountsJourney.AccountDetailsModel, AccountsJourney.ErrorResponse>) -> Void) {
+        if shouldReturnError {
+            completion(.failure(AccountsJourney.ErrorResponse(statusCode: 404, data: nil, error: AccountDetails.Error.invalidResponse)))
+        } else {
+            let data = Bundle.data(from: fileName)
+            
+            guard let arrangementDetails = try? JSONDecoder().decode(ArrangementsClient2Gen2.AccountArrangementItem.self, from: data) else {
+                completion(.failure(.init()))
+                return
+            }
+            
+            completion(.success(arrangementDetails.toDomainModel()))
+        }
+    }
+       
+}
+
