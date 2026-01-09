4233cf2500849e2aeeb2668bc4bde1bf545e1726
Sadiq Mohammad <sadiq@backbase.com>
2026-01-09 11:27:25 +0530

    MF2425: Updated Backbase server URLs

--- appCommon/Configuration/BBConfigurationConstants.swift
+++ appCommon/Configuration/BBConfigurationConstants.swift
@@ -8,8 +8,11 @@
 struct BBConfigurationConstants {
     static let version = "6.1.5"
     static var apiKey = "Add your API key here"
-    static let identityBaseURL = "https://identity.dev.sdbxaz.azure.backbaseservices.com"
-    static let backbaseServerURL = "https://app.dev.sdbxaz.azure.backbaseservices.com"
+    static let identityBaseURL = "https://identity.prd.sdbxaz.azure.backbaseservices.com"
+    static let backbaseServerURL = "https://app.prd.sdbxaz.azure.backbaseservices.com"
+    static let peristentHeaders = [
+        "X-User-Context": ["backbaseservices.com"]
+    ]
     static let realm = "retail"
     static let clientId = "mobile-client"
     static let applicationKey = "retail"
